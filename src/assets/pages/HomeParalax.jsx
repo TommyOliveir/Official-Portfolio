2a0ceb3a4c8bcb21cd9720e1638305db22ec9cd5
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-04-14 22:56:14 +0200

    css update

--- src/assets/pages/HomeParalax.jsx
+++ src/assets/pages/HomeParalax.jsx
@@ -5,25 +5,28 @@ function HomeParalax() {
   return (
     <>
       <section className="bg-red-200 h-screen flex ">
-        <div className="bg-blue-300 p-44">
-          <h1 className="text-6xl font-semibold text-gray-800 uppercase ">
-            Hi, I am Tommy
-          </h1>
-          <h2 className="text-xl tracking-wide text-gray-700 uppercase mt-4">
-            Front-End React Developer
-          </h2>
-          <Link
-            className="inline-block mt-8 py-2 px-4 rounded bg-orange-400 text-white
-           hover:border-orange-400
-           hover:border 
-           hover:text-orange-400
-           hover:bg-white"
-            to="/"
-          >
-            See my projects
-          </Link>
+        <div className="w-1/2">
+          <div className="bg-blue-300 md:pl-44 md:pt-44">
+            <h1 className="text-6xl font-semibold text-gray-800 uppercase ">
+              Hi, I am Tommy
+            </h1>
+            <h2 className="text-xl tracking-wide text-gray-700 uppercase mt-4">
+              Front-End React Developer
+            </h2>
+            <Link
+              className="inline-block mt-8 py-2 px-4 rounded bg-orange-400 text-white 
+                hover:border-orange-400
+                hover:border
+                hover:text-orange-400
+                hover:bg-white"
+              to="/"
+            >
+              See my projects
+            </Link>
+          </div>
         </div>
-        <div>image</div>
+
+        <div className="w-1/2 bg-red-600">image</div>
       </section>
     </>
   );
