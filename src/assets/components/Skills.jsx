e21f4d5d1114b2831ed6667caa2c936ea6aa797e
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-04-21 17:25:51 +0200

    skills

--- src/assets/components/Skills.jsx
+++ src/assets/components/Skills.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faHtml5, faCss3, faSquareJs, faReact, faSass, faGit, faGithub  } from '@fortawesome/free-brands-svg-icons'
+
+
+
 
 function Skills() {
   return (
     <section>
-      <div>Skills</div>
+      <div className="skill-details border-4 border-green-400 ">
+        <h2>Skill</h2>
+        <div className="flex flex-wrap gap-4 justify-center ">
+       
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faHtml5} /></div>
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faCss3} /></div>
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faSquareJs } /></div>
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faReact } /></div>
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faSass } /></div>
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faGit } /></div>
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faGithub  } /></div>
+          <div className="text-center p-4 bg-red-400 w-72"><FontAwesomeIcon icon={faSass } /></div>
+        
+        </div>
+      </div>
     </section>
   );
 }
