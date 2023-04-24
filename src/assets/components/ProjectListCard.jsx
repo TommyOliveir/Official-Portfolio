07f5af54420bfe3ffe27cbfbe822c32c0d675463
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-04-24 22:04:14 +0200

    okay

--- src/assets/components/ProjectListCard.jsx
+++ src/assets/components/ProjectListCard.jsx
@@ -3,16 +3,17 @@ import React from "react";
 function ProjectListCard(props) {
   return (
     <>
-      <div className="w-full sm:w-1/4   m-4 flex flex-col justify-start items-start rounded overflow-hidden border border-gray-300">
+      <div className="w-full sm:w-1/4 h-auto  m-4 flex flex-col justify-start items-start rounded overflow-hidden border border-gray-300">
         <div className="w-full bg-blue-400 h-40 ">image</div>
-        <div className="p-4 border  h-full flex flex-col flex-wrap w-full">
-          <h2> {props.name}</h2>
-          <p>{props.description}</p>
-          <button className="bg-orange-300 px-4 py-1 rounded mt-4">View on Github</button>
-         
+        <div className="p-4 border h-full flex justify-between  flex-col flex-wrap w-full">
+          <div>
+            <h2> {props.name}</h2>
+            <p>{props.description}</p>
+          </div>
+          <button className="bg-orange-300 px-4 py-1 rounded self-start">
+            View on Github
+          </button>
         </div>
-
-        
       </div>
     </>
   );
