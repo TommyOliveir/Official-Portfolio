162c2924debe859548154840f81a3b9cdda7e5e3
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-04-24 20:22:42 +0200

    good

--- src/assets/components/ProjectDetail.jsx
+++ src/assets/components/ProjectDetail.jsx
@@ -3,20 +3,15 @@ import React from "react";
 function ProjectDetail(props) {
   return (
     <>
-     
-        <div className="w-32 sm:w-1/4 h-32 p-4 bg-red-500 m-4 flex flex-col justify-between items-center text-center">
-          <div className="w-full bg-blue-400 h-16">
-            image
-          </div>
-          {props.name}
-
-          <div className="self-start">
-
-          <button>preview</button>
-          </div>
+      <div className="w-32 sm:w-1/4 h-32  bg-red-500 m-4 flex flex-col justify-between items-start">
+        <div className="w-full bg-blue-400 h-16">image</div>
+        <div>
+          <h2> {props.name}</h2>
+          <button className="bg-green-500">preview</button>
         </div>
-     
-     
+
+        
+      </div>
     </>
   );
 }
