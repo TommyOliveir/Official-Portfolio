f7bf774e8cd6147e7fa2c49318946b77be1bc3bb
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-05-24 19:10:29 +0200

    debug

--- src/assets/components/T.jsx
+++ src/assets/components/T.jsx
@@ -1,32 +1,9 @@
-import React, { useState } from "react";
+import React from 'react'
 
 function T() {
-  const [text, setText] = useState("");
-  const [lineCount, setLineCount] = useState(1);
-
-  const handleTextChange = (event) => {
-    const value = event.target.value;
-    const lineBreaks = (value.match(/\n/g) || []).length;
-    const lines = value.split("\n").length;
-    if(lineBreaks) {
-      setLineCount(prev => prev + lineBreaks)
-    }
-    setLineCount(lines);
-    setText(value);
-  };
-
   return (
-    <div>
-      <textarea
-        value={text}
-        onChange={handleTextChange}
-        rows={lineCount}
-        cols={30}
-        style={{ resize: "none" , height: "400px"}}
-      />
-      <p>Line count: {lineCount}</p>
-    </div>
-  );
+    <div>T</div>
+  )
 }
 
-export default T;
+export default T
