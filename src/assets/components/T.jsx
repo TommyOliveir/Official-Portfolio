6964d8abf626ddd1df1da90ceacc1e11a84b4c24
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-05-26 18:32:47 +0200

    test

--- src/assets/components/T.jsx
+++ src/assets/components/T.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useRef, useEffect} from 'react';
 
-function T() {
+const T = () => {
+  const [text, setText] = useState('');
+  const newlineCount = (text.match(/\n/g) || []).length;
+  const textareaRef = useRef(null);
+  const [isWidthFilled, setIsWidthFilled] = useState(true);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+
+   
+  };
+
+
+
+
+
   return (
-    <div>T</div>
-  )
-}
+    <div>
+      <textarea wrap="hard" ref={textareaRef} className='textareacount' value={text} onChange={handleChange} style={{width: "200px", height: "100px"}}/>
+      <p>Newline count: {newlineCount}</p>
+      <p>{isWidthFilled ? 'Text width is filled' : 'Text width is not filled'}</p>
+    </div>
+  );
+};
 
-export default T
+export default T;
