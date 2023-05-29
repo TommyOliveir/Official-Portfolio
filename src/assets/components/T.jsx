7481a52d6efd388a5ab69204fe7737139095c3e3
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-05-29 22:06:55 +0200

    err

--- src/assets/components/T.jsx
+++ src/assets/components/T.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function T() {
+const T = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    textArea: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Create a new instance of FormData
+    const data = new FormData();
+    data.append("firstName", formData.firstName);
+    data.append("text", formData.textArea);
+    console.log(formData);
+
+    // Perform your form submission or data handling here
+    // For example, you can make an AJAX request with the form data
+    // using fetch() or an HTTP client library like Axios.
+
+    // Reset the form after submission
+    setFormData({
+      firstName: "",
+      textArea: "",
+    });
+  };
+
   return (
-    <div>T</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
+      <label>
+        textArea
+        <textarea
+          wrap="hard"
+          style={{ wordBreak: "break-word" }}
+          name="textArea"
+          value={formData.textArea}
+          onChange={handleInputChange}
+        />
+      </label>
+      <br />
 
-export default T
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default T;
