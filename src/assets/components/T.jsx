8404e400003ad58edce97c838eba079f2fb86427
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-05-31 15:12:28 +0200

    solve

--- src/assets/components/T.jsx
+++ src/assets/components/T.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 
-function T() {
+const T = () => {
+  const textareaRef = useRef(null);
+  const [rowsCount, setRowscount] = useState(0);
+
+  const handleInputChange = () => {
+    const textarea = textareaRef.current;
+    const lineHeight = parseInt(getComputedStyle(textarea).lineHeight);
+    const scrollHeight = textarea.scrollHeight;
+    console.log("Line height:", lineHeight);
+    console.log("Scroll height:", scrollHeight);
+    const lineUsed = scrollHeight / lineHeight;
+    setRowscount(lineUsed);
+
+    const content = textareaRef.current.innerHTML;
+    console.log('Content:', content);
+  };
+
   return (
-    <div>T</div>
-  )
-}
+    <>
+      <div contenteditable="true"
+      
+        ref={textareaRef}
+        // onChange={handleInputChange}
+        onInput={handleInputChange}
+        style={{ resize: "none", width: "200px" }}
+      />
+      rows{rowsCount}
+    </>
+  );
+};
 
-export default T
+export default T;
