efcddd7c309a4103b35fcf74f66f130f5dca2d22
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-10-26 21:04:32 +0200

    update

--- src/assets/components/Modal.jsx
+++ src/assets/components/Modal.jsx
@@ -4,7 +4,7 @@ export default function Modal({ onClose }) {
   return (
     <div className="modal fixed inset-0 flex items-center justify-center z-50">
       <div className="modal-overlay bg-black opacity-80 inset-0 fixed"></div>
-      <div className="modal-content h-1/2 bg-white w-1/2 p-8 drop-shadow-md ">
+      <div className="modal-content h-1/2 bg-white md:w-1/2 p-8 drop-shadow-md ">
         <div>MODAL</div>
         <YouTubeVideo />
         <button className="bg-red-500 px-3 text-white mt-2" onClick={onClose}>
